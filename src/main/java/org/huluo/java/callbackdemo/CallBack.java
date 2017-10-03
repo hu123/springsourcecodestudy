4987a74a23b8b99626b453af61e3de47c5cc6e9b
huluo <huyunluo@gmail.com>
2017-10-03 15:57:22 +0800

    feat:Java回调机制demo

---
+++ src/main/java/org/huluo/java/callbackdemo/CallBack.java
@@ -0,0 +1,9 @@
+package org.huluo.java.callbackdemo;
+
+public interface CallBack {
+    /**
+     * 这个是小李知道答案时要调用的函数告诉小王，也就是回调函数
+     * @param result 是答案
+     */
+    void solve(String result);
+}
