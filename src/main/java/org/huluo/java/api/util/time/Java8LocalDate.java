b91e7f58d8f9eca0c4f8524b1b4f8c2fd09b04ba
huluo <huyunluo@gmail.com>
2017-11-03 21:58:30 +0800

    feat: java8 LocalDate api demo

--- src/main/java/org/huluo/java/api/util/time/Java8LocalDate.java
+++ src/main/java/org/huluo/java/api/util/time/Java8LocalDate.java
@@ -1,7 +1,33 @@
 package org.huluo.java.api.util.time;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.chrono.Era;
+
 public class Java8LocalDate {
     public static void main(String[] args) {
+        new Java8LocalDate().localDate();
+    }
+
+
+    private void localDate() {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        localDate = LocalDate.ofYearDay(2005, 86);
+        System.out.println(localDate);
+
+        LocalDate localDate1 = LocalDate.of(2013, Month.AUGUST, 10);
+
+        System.out.println(localDate.getDayOfWeek());
+
+
+        System.out.println(LocalDate.ofEpochDay(-1));
+
+        System.out.println(localDate1.isBefore(localDate));
+
+        Era era = localDate.getEra();
+        System.out.println(era);
 
     }
 }
