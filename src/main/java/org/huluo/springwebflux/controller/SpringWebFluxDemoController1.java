b42b82080cce788128a47cc3f890303edc74c042
huluo <huyunluo@gmail.com>
2017-11-06 21:40:52 +0800

    feat:delayMono

--- src/main/java/org/huluo/springwebflux/controller/SpringWebFluxDemoController1.java
+++ src/main/java/org/huluo/springwebflux/controller/SpringWebFluxDemoController1.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 /**
  * @see <a href="http://www.infoq.com/cn/articles/reactor-by-example">http://www.infoq.com/cn/articles/reactor-by-example</a>
  */
@@ -16,6 +18,12 @@ public class SpringWebFluxDemoController1 {
     @ResponseBody
     public Mono<String> sayHello(@PathVariable String who) {
         return Mono.just(who).map(s -> "my name is" + who);
+    }
+
+    @ResponseBody
+    @RequestMapping("delayMono")
+    public Mono<String> delayMono() {
+        return Mono.just("myName is huluo ").delaySubscription(Duration.ofSeconds(20));
     }
 
 }
